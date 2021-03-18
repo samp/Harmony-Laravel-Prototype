09690088c4f3a888c50a30c66971a23d5f0941cd
samp <isambarding@gmail.com>
2021-03-18 16:25:53 +0000

    Admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,4 +10,29 @@ class AdminController extends Controller
     {
         return view('admin.index');
     }
+
+    public function create()
+    {
+        return view('admin.create');
+    }
+
+    public function store()
+    {
+        return view('admin.store');
+    }
+
+    public function show()
+    {
+        return view('admin.show');
+    }
+
+    public function update()
+    {
+        return view('admin.update');
+    }
+
+    public function destroy()
+    {
+        return view('admin.destroy');
+    }
 }
