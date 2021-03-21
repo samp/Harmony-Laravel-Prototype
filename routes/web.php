86843c50822841de48d8995f1d9ae2a6bcf78b9d
samp <isambarding@gmail.com>
2021-03-21 19:05:17 +0000

    frontend

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
+Route::get('/music', [App\Http\Controllers\SearchController::class, 'index'])->name('music');
+Route::get('/events', [App\Http\Controllers\EventController::class, 'index'])->name('events');
+Route::get('/merch', [App\Http\Controllers\SearchController::class, 'index'])->name('merch');
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 
 require __DIR__.'/auth.php';
