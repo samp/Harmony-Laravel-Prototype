d77f7d0990de8b673fbce6765b57d4eacc131de9
samp <isambarding@gmail.com>
2021-03-25 20:43:01 +0000

    Events frontend

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,26 +1,35 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="mt-5">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        @foreach($events as $event)
-            <div class="flex flex-col md:flex-row bg-white rounded-xl shadow overflow-hidden mb-4">
-                <img alt="{{ $event->description }}" src="/storage/{{ $event->image }}" class="w-full md:w-64">
-                <div class="p-4">
-                    <h2 class="font-bold text-xl text-gray-800">{{ $event->artist }} - {{ $event->name }} </h2>
-                    <h3 class="text-sm text-gray-600 pb-4">{{ \Carbon\Carbon::parse($event->time)->format('jS F Y - h:m') }} @ {{ $event->location }}</h3>
-                    <p class="text-gray-700">{{ $event->description }}</p>
-                </div> 
-            </div>
-        @endforeach
-
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-4">
+            @foreach ($events as $event)
+                <div class="flex flex-row md:flex-col lg:flex-row bg-white rounded-xl shadow overflow-hidden">
+                    <picture class="w-52 md:w-full lg:w-52">
+                        <img alt="{{ $event->description }}" src="/storage/{{ $event->image }}"
+                            class="w-full h-full object-cover md:w-64">
+                    </picture>
+                    <div class="flex flex-col p-4">
+                        <h3 class="font-medium text-xl font-poppins">{{ $event->name }}
+                        </h3>
+                        <h3 class="font-medium text-lg font-poppins">{{ $event->artist }}
+                        </h3>
+                        <div class="py-2">
+                            <p>
+                                {{ \Carbon\Carbon::parse($event->time)->format('jS F Y - h:i A') }}</p>
+                            <p>{{ $event->location }}</p>
+                        </div>
+                        <div
+                            class="text-right mt-auto ml-auto w-auto rounded-md border-2 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors">
+                            <a href="{{ route('events') . '/' . $event->id }}"
+                                class="flex flex-row align-middle p-2 font-poppins"><span class="mx-2 text-xl">More
+                                </span><i class="fas fa-arrow-right text-xl leading-7" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
         <div class="py-4">
-             {{ $events->links() }}
-            </div>
+            {{ $events->links() }}
         </div>
     </div>
+
 </x-app-layout>
--- resources/views/merch/show.blade.php
+++ resources/views/merch/show.blade.php
@@ -1,17 +1,50 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col-reverse sm:grid sm:grid-cols-3 md:grid-cols-4 mt-4 gap-4">
-
-                {{ $itemlisting }}
-
+    <div class="max-w-7xl w-full mx-auto sm:px-6 lg:px-8">
+        <div class="flex flex-col sm:flex-row bg-white rounded-xl shadow overflow-hidden">
+            <div class="h-full sm:max-w-sm">
+                <picture>
+                    <source srcset="{{ '../storage/' . $itemlisting->product_image }}">
+                    <img class="w-full h-auto object-cover" src="{{ '../storage/' . $itemlisting->product_image }}"
+                        alt="{{ $itemlisting->name }}'s cover image">
+                </picture>
             </div>
+
+            <div class="flex flex-col md:w-2/3 p-6">
+                <div class="flex flex-col text-lg mb-6">
+                    <h1 class="text-4xl mb-2 font-medium font-poppins">
+                        {{ $itemlisting->name }}
+                    </h1>
+                    <h2 class="text-3xl font-poppins">{{ $itemlisting->keywords }}</h2>
+                </div>
+
+                <div class="flex flex-col h-full">
+                    <p>{{ $itemlisting->description }}</p>
+                    <div class="flex flex-row justify-between">
+                        <div class="grid grid-cols-2 items-center">
+                            <label for="size">Size:</label>
+                            <select name="size" id="size" class="rounded-lg shadow border-gray-300">
+                                @foreach ($itemlisting->items as $item)
+                                    <option value="{{ $item->id }}">
+                                        {{ $item->size }}</option>
+                                @endforeach
+                            </select>
+
+                            @error('size')
+                                <p>{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                    </div>
+                    <div
+                        class="mt-auto rounded-md border-2 w-auto ml-auto border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-white transition-colors">
+                        <a href="#" class="flex flex-row align-middle p-2 font-poppins"><span class="mx-2 text-xl">Add
+                                to cart
+                            </span></a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
