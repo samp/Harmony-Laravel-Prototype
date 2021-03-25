f3de293184d2ac92dcd68f0d4490a3f03f05c277
samp <isambarding@gmail.com>
2021-03-25 16:50:38 +0000

    admin frontend

--- resources/views/admin/disc/create.blade.php
+++ resources/views/admin/disc/create.blade.php
@@ -1,31 +1,27 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="mt-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex flex-col sm:flex-row bg-white rounded-xl shadow overflow-hidden">
                 <div class="p-6">
-                    <h1>Create Disc</h1>
-                    <form method="post">
+                    <h1 class="text-3xl font-poppins mb-2">Create Disc</h1>
+                    <form method="post" class="flex flex-col gap-4">
                         @csrf
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="name">Name:</label>
-                            <input type="text" name="name" id="name" value="{{ old('name') }}">
+                            <input type="text" name="name" id="name" value="{{ old('name') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('name')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="album_id">Album:</label>
-                            <select name="album_id" id="album_id">
-                                @foreach($albums as $album)
-                                    <option value="{{ $album->id }}">{{ $album->albumListing->name }} - {{ $album->format }}</option>
+                            <select name="album_id" id="album_id" class="rounded-lg shadow border-gray-300">
+                                @foreach ($albums as $album)
+                                    <option value="{{ $album->id }}">{{ $album->albumListing->name }} -
+                                        {{ $album->format }}</option>
                                 @endforeach
                             </select>
 
@@ -34,7 +30,9 @@
                             @enderror
                         </div>
 
-                        <button type="submit" name="submit">Create</button>
+                        <button type="submit" name="submit"
+                            class="ml-auto w-auto rounded-md border-2 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors flex align-middle p-2 font-poppins"><span
+                                class="mx-2 text-xl">Create</span></button>
                     </form>
                 </div>
             </div>
--- resources/views/admin/event/create.blade.php
+++ resources/views/admin/event/create.blade.php
@@ -1,63 +1,62 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="mt-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex flex-col sm:flex-row bg-white rounded-xl shadow overflow-hidden">
                 <div class="p-6">
-                    <h1>Create Event</h1>
-                    <form method="post" enctype="multipart/form-data">
+                    <h1 class="text-3xl font-poppins mb-2">Create Event</h1>
+                    <form method="post" enctype="multipart/form-data" class="flex flex-col gap-4">
                         @csrf
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="name">Name:</label>
-                            <input type="text" name="name" id="name" value="{{ old('name') }}">
+                            <input type="text" name="name" id="name" value="{{ old('name') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('name')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="artist">Artist:</label>
-                            <input type="text" name="artist" id="artist" value="{{ old('artist') }}">
+                            <input type="text" name="artist" id="artist" value="{{ old('artist') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('artist')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="description">Description:</label>
-                            <input type="text" name="description" id="description" value="{{ old('description') }}">
+                            <input type="text" name="description" id="description" value="{{ old('description') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('description')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="time">Time:</label>
-                            <input type="datetime-local" name="time" id="time" value="{{ old('time') }}">
+                            <input type="datetime-local" name="time" id="time" value="{{ old('time') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('time')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="location">Location:</label>
-                            <input type="text" name="location" id="location" value="{{ old('location') }}">
+                            <input type="text" name="location" id="location" value="{{ old('location') }}"
+                                class="rounded-lg shadow border-gray-300">
 
                             @error('location')
                                 <p>{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div>
+                        <div class="grid grid-cols-2 items-center">
                             <label for="image">Image:</label>
                             <input type="file" name="image" id="image">
 
@@ -66,7 +65,9 @@
                             @enderror
                         </div>
 
-                        <button type="submit" name="submit">Create</button>
+                        <button type="submit" name="submit"
+                        class="ml-auto w-auto rounded-md border-2 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors flex align-middle p-2 font-poppins"><span
+                            class="mx-2 text-xl">Create</span></button>
                     </form>
                 </div>
             </div>
