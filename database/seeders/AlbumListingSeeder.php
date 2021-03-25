e7d1fb6d1e54cd75331454eb11af87a3a7c6ba35
Kieran Noble <kieran@aylo.net>
2021-03-25 18:17:34 +0000

    added more album seeds

--- database/seeders/AlbumListingSeeder.php
+++ database/seeders/AlbumListingSeeder.php
@@ -68,6 +68,61 @@ class AlbumListingSeeder extends Seeder
             'release_date' => \Carbon\Carbon::parse('2019-11-29'),
             'cover_image' => "static/born2rap.jpg",
         ]);
+        DB::table('album_listings')->insert([
+            'id' => 7,
+            'name' => "Positions",
+            'artist' => "Ariana Grande",
+            'description' => "Positions is the sixth studio album by American singer Ariana Grande. It was released by Republic Records on October 30, 2020. Grande worked with a variety of producers on the album, including Tommy Brown, Anthony M. Jones, London on da Track, Murda Beatz, the Rascals, Scott Storch, Shea Taylor and Charles Anderson, accompanied by longtime co-writers Victoria Monét and Tayla Parx.",
+            'genre' => "RnB",
+            'release_date' => \Carbon\Carbon::parse('2020-10-30'),
+            'cover_image' => "static/positions.jpg",
+        ]);
+        DB::table('album_listings')->insert([
+            'id' => 8,
+            'name' => "thank u, next",
+            'artist' => "Ariana Grande",
+            'description' => "Thank U, Next (stylized in all lowercase) is the fifth studio album by American singer Ariana Grande, released on February 8, 2019, by Republic Records.[5][6] It was released six months after her fourth studio album Sweetener (2018), created in the midst of Grande's personal struggles, including the death of ex-boyfriend Mac Miller and her breakup with then-fiancé Pete Davidson.",
+            'genre' => "Pop",
+            'release_date' => \Carbon\Carbon::parse('2019-02-08'),
+            'cover_image' => "static/thankunext.jpg",
+        ]);
+        DB::table('album_listings')->insert([
+            'id' => 9,
+            'name' => "Sweetener",
+            'artist' => "Ariana Grande",
+            'description' => "Sweetener is the fourth studio album by American singer Ariana Grande. It was released on August 17, 2018, through Republic Records. Sweetener is primarily a pop, R&B and trap album, while incorporating a diversity of other music genres, including house, funk, neo soul, hip hop, synth-pop and other minimalist urban influences.",
+            'genre' => "Pop",
+            'release_date' => \Carbon\Carbon::parse('2018-08-17'),
+            'cover_image' => "static/sweetener.jpg",
+        ]);
+        DB::table('album_listings')->insert([
+            'id' => 10,
+            'name' => "Dangerous Woman",
+            'artist' => "Ariana Grande",
+            'description' => "Dangerous Woman is the third studio album by American singer Ariana Grande, that was released by Republic Records on May 20, 2016. The album features guest appearances from Nicki Minaj, Lil Wayne, Macy Gray and Future. Dangerous Woman is primarily a pop and R&B record, with influences of dance-pop, disco, house, trap, and reggae genres.",
+            'genre' => "Pop",
+            'release_date' => \Carbon\Carbon::parse('2016-05-20'),
+            'cover_image' => "static/dangerouswoman.jpg",
+        ]);
+        DB::table('album_listings')->insert([
+            'id' => 11,
+            'name' => "Folklore",
+            'artist' => "Taylor Swift",
+            'description' => "Eighth studio album by the American singer-songwriter. The album was created in isolation during the COVID-19 pandemic, with production from Aaron Dessner (The National), Jack Antonoff (Bleachers) and Swift herself. Departing from the mainstream pop sound of Swift's previous releases, 'Folklore' is an indie folk, alternative rock, electro-folk and chamber pop album driven by pianos and guitars.",
+            'genre' => "Pop",
+            'release_date' => \Carbon\Carbon::parse('2020-08-07'),
+            'cover_image' => "static/folklore.jpg",
+        ]);
+        DB::table('album_listings')->insert([
+            'id' => 12,
+            'name' => "Glory",
+            'artist' => "Britney Spears",
+            'description' => "Ninth studio album from the American pop singer. Featuring the singles 'Make Me... (feat. G-Eazy)' and 'Slumber Party', this release debuted in the UK Albums Chart at #2. The deluxe edition includes an additional five bonus tracks.",
+            'genre' => "Pop",
+            'release_date' => \Carbon\Carbon::parse('2016-08-26'),
+            'cover_image' => "static/glory.jpg",
+        ]);
+
         
     }
 }
