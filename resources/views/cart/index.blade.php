2f3ecfb523c086be1fcc585ff139882f6b1d85c9
Kieran Noble <kieran@aylo.net>
2021-04-17 15:25:54 +0100

    Fix incorrect key mapping

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -7,7 +7,7 @@
         <x-card class="flex-col justify-between p-6">
             <h2 class="text-3xl font-poppins">Cart</h2>
             <div class="">
-                @forelse($cart as $cartitem)
+                @forelse($cart as $key => $cartitem)
                 <div class="flex">
                     <div class="flex-grow pr-4">
                         <div class="flex">
@@ -19,7 +19,7 @@
                          
                     </div>
 
-                    <form action="/cart/remove/{{ $loop->index }}" method="POST">
+                    <form action="/cart/remove/{{ $key }}" method="POST">
                         @csrf
                         <button>
                             <i class="text-red-300 fas fa-trash-alt hover:bg-truegray-700 hover:text-red-400 p-2 rounded"></i>
