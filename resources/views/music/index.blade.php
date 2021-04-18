3a5ca31d5697d3ff5d3f9107e0b415eef101a31e
samp <isambarding@gmail.com>
2021-04-18 10:12:17 +0100

    more button fixes

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -27,7 +27,8 @@
         </div>
         <div class="col-start-4 relative sm:ml-auto text-gray-600 dark:text-truegray-100">
             <form>
-                <input class="bg-white dark:bg-truegray-600 dark:placeholder-truegray-100 w-full border-none h-10 px-5 pr-16 rounded-lg shadow text-sm focus:outline-none"
+                <input
+                    class="bg-white dark:bg-truegray-600 dark:placeholder-truegray-100 w-full border-none h-10 px-5 pr-16 rounded-lg shadow text-sm focus:outline-none"
                     type="search" name="search" placeholder="Search" value="{{ Request()->input('search') }}">
                 <button type="submit" class="absolute right-0 h-10 px-4 py-1">
                     <i class="fas fa-search leading-7" aria-hidden="true"></i>
@@ -59,12 +60,10 @@
                             </div>
                         @endforeach
                     </div>
-                    <div
-                        class="text-right mt-auto ml-auto w-24 rounded-md border-2 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors">
-                        <a href="{{ route('music') . '/' . $albumlisting->id }}"
-                            class="flex flex-row align-middle p-2"><span class="m-auto text-xl">Buy
-                            </span><i class="fas fa-arrow-right text-xl leading-7" aria-hidden="true"></i></a>
-                    </div>
+
+                    <x-button-link :href="route('music') . '/' . $albumlisting->id" class="mt-auto ml-auto">
+                        {{ 'Buy' }}
+                    </x-button-link>
 
                 </div>
 
