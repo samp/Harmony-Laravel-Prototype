4c07c9f8927a838d962e08159a9f8665748de2b4
samp <isambarding@gmail.com>
2021-04-18 10:13:17 +0100

    transition duration

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'font-poppins ml-auto px-4 py-2 text-xl w-auto rounded-lg text-white bg-purple-500 dark:bg-purple-600 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'font-poppins ml-auto px-4 py-2 text-xl w-auto rounded-lg text-white bg-purple-500 dark:bg-purple-600 border-purple-400 bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 hover:text-white hover:border-transparent transition-colors duration-200']) }}>
     {{ $slot }}
 </button>
